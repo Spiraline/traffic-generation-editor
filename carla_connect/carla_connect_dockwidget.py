bfd6ebd5f01ab0addd75fed1a3661c31cb8593fa
Spiraline <spiraline@rideflux.com>
2024-10-21 13:35:12 +0900

    Don't terminate QGIS when play scenario failed

--- carla_connect/carla_connect_dockwidget.py
+++ carla_connect/carla_connect_dockwidget.py
@@ -123,5 +123,6 @@ class CarlaConnectDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         '''
         ImageProcessor.destroy_all_window()
         if self._scenario_runner_process.poll() is None:
+            self._scenario_runner_process.communicate()
             self._scenario_runner_process.kill()
             self._scenario_runner_process.communicate()
